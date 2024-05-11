97c14bab9fd200e6eab4cb816da906a60d5bdfd6
GIGABYTE-PC92\Gigabyte <mrukropus@gmail.com>
2024-05-11 20:41:23 +0300

    Method1

---
+++ src/Method1.java
@@ -0,0 +1,15 @@
+public class Method1 {
+
+
+    public static void main(String[] args) {
+        printThreeWords();
+
+
+    }
+
+    static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple ");
+    }
+
+
+}
