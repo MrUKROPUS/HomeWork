64b1caf288f18f77d9221df288fbc8c4ceda6ed4
GIGABYTE-PC92\Gigabyte <mrukropus@gmail.com>
2024-05-15 11:22:38 +0300

    method8

--- src/MethodsJava.java
+++ src/MethodsJava.java
@@ -2,10 +2,15 @@ public class MethodsJava {
 
     public static void main(String[] args) {
         printThreeWords();
-        checkSumSign ();
-
+        checkSumSign();
+        printColor();
+        compareNumbers();
+        checkingAmountsWithinLimits(5, 20);
+        wholeNumber(-5);
+        wholeNumberTrueFalse(-10);
 
     }
+
 
     private static void printThreeWords() {
         System.out.println("Orange\nBanana\nApple");
@@ -13,8 +18,7 @@ public class MethodsJava {
 
     }
 
-    public static void checkSumSign()
-    {
+    public static void checkSumSign() {
         int a = 15;
         int b = -10;
 
@@ -26,6 +30,57 @@ public class MethodsJava {
             System.out.println("Сумма отрицательная");
         }
 
+
     }
 
+    public static void printColor() {
+        int value = 150;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value >= 0 && value <= 50) {
+            System.out.println("Жёлтый");
+        } else {
+            System.out.println("Зелёный");
+        }
+
+    }
+
+    public static void compareNumbers() {
+        int a = 50;
+        int b = 150;
+        if (a >= b) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
+    }
+
+    public static void checkingAmountsWithinLimits(int a, int b) {
+        int sum = a + b;
+        if (sum > 10 && sum <= 20) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+    }
+
+    public static void wholeNumber(int number) {
+        if (number >= 0) {
+            System.out.println("Positive number");
+        } else {
+            System.out.println("Negative number");
+        }
+
+
+    }
+
+    public static boolean wholeNumberTrueFalse (int number) {
+         boolean b = number < 0;
+        System.out.println(b);
+        return b;
+
+
+    }
 }
+
