36f3e2cd0bb7383ddb6222cd0ad78a0de2e01947
GIGABYTE-PC92\Gigabyte <mrukropus@gmail.com>
2024-05-15 12:41:27 +0300

    method14

--- src/MethodsJava.java
+++ src/MethodsJava.java
@@ -8,6 +8,13 @@ public class MethodsJava {
         checkingAmountsWithinLimits(5, 20);
         wholeNumber(-5);
         wholeNumberTrueFalse(-10);
+        stringAndNumber("Я не буду зря тратить место в коде", 3);
+        steklovataNoviyGod(1900);
+        integerArray();
+        integerArrayPartTwo();
+        integerArrayTheThird();
+        squareTwoDimensionalIntegerArray();
+        twoArgumentMethod(10,15);
 
     }
 
@@ -75,12 +82,100 @@ public class MethodsJava {
 
     }
 
-    public static boolean wholeNumberTrueFalse (int number) {
-         boolean b = number < 0;
+    public static boolean wholeNumberTrueFalse(int number) {
+        boolean b = number < 0;
         System.out.println(b);
         return b;
 
 
+    }
+
+    public static void stringAndNumber(String str, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println(str);
+        }
+
+    }
+
+    public static boolean steklovataNoviyGod(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else {
+            return year % 4 == 0;
+        }
+    }
+
+    public static void integerArray() {
+        int array[] = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                array[i] = 1;
+            } else {
+                array[i] = 0;
+            }
+        }
+        for (int number : array) {
+            System.out.println(number + " ");
+        }
+        System.out.println();
+    }
+
+    public static void integerArrayPartTwo() {
+        int[] array = new int[100];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    public static void integerArrayTheThird() {
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] *= 2;
+            }
+        }
+
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void squareTwoDimensionalIntegerArray() {
+        int n = 3;
+
+        int[][] array = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            array[i][i] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[] twoArgumentMethod(int len, int initialValue) {
+        int[] array = new int[len];
+        for (int i = 0; i < len; i++) {
+            array[i] = initialValue;
+        }
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        return array;
     }
 }
 
