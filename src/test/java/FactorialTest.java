d128a70fba320ea18d8a85640ed667aa29101aa7
GIGABYTE-PC92\Gigabyte <mrukropus@gmail.com>
2024-06-24 14:01:29 +0300

    ...

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,33 +1,30 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 
 public class FactorialTest {
 
     @Test
     public void testFactorialOfZero() {
-        assertEquals(1, Factorial.factorial(0), "Факториал нуля должен быть равен 1");
+        assertEquals(Factorial.factorial(0), 1, "Факториал нуля должен быть равен 1");
     }
 
     @Test
     public void testFactorialOfOne() {
-        assertEquals(1, Factorial.factorial(1), "Факториал единицы должен быть равен 1");
+        assertEquals(Factorial.factorial(1), 1, "Факториал единицы должен быть равен 1");
     }
 
     @Test
     public void testFactorialOfPositiveNumber() {
-        assertEquals(120, Factorial.factorial(5), "Факториал пяти должен быть равен 120");
+        assertEquals(Factorial.factorial(5), 120, "Факториал пяти должен быть равен 120");
     }
 
-    @Test
+    @Test(expectedExceptions = IllegalArgumentException.class)
     public void testFactorialOfNegativeNumber() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            Factorial.factorial(-1);
-        });
-        assertEquals("Факториал не определен для отрицательных чисел.", exception.getMessage());
+        Factorial.factorial(-1);
     }
 
     @Test
     public void testFactorialOfLargeNumber() {
-        assertEquals(3628800, Factorial.factorial(10), "Факториал десяти должен быть равен 3628800");
+        assertEquals(Factorial.factorial(10), 3628800, "Факториал десяти должен быть равен 3628800");
     }
 }
